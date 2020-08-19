b7da9ca5d0a34e90b975a63fae48e2babea9936a
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-08-19 00:01:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import time, sys
+
+green = "\033[0;32m"
+red = "\033[0;31m"
+
+def typewriter(message):
+	for i in message:
+		sys.stdout.write(i)
+		sys.stdout.flush()
+		if ((i != "\n") and (i != ":")):
+			time.sleep(0.08)
+		else:
+			time.sleep(0.9)
+
+
+message = (red + 'We meet again, world. My name is Mihir. I live in an undisclosed location.')
+
+typewriter(message)
