a161b75e15310f76d0bd22086dfadffd5dd32517
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-08-19 00:09:16 +0000

    added more text colors.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import time, sys
 
+#colors 
+
 green = "\033[0;32m"
 red = "\033[0;31m"
+yellow = "\033[0;33m"
+blue = "\033[0;34m"
+magenta = "\033[0;35m"
+cyan = "\033[0;36m"
+white = "\033[0;37m"
+bright_black = "\033[0;90m"
+bright_red = "\033[0;91m"
+bright_green = "\033[0;92m"
+bright_yellow = "\033[0;93m"
+bright_blue = "\033[0;94m"
+bright_magenta = "\033[0;95m"
+bright_cyan = "\033[0;96m"
+bright_white = "\033[0;97m"
 
 def typewriter(message):
 	for i in message:
@@ -12,11 +27,3 @@ def typewriter(message):
 		else:
 			time.sleep(0.9)
 
-
-message = (red + 'We meet again, world. My name is Mihir. I live in an undisclosed location.')
-
-typewriter(message)
-
-message2 = (green + 'waddup world')
-
-typewriter(message2)
